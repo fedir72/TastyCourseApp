c9217d8605cdeb38dbe7b93f1513c27729ab36ed
fedir72 <fedir72@ua.fm>
2022-11-17 23:10:15 +0200

    + custom colours and background image, registration screen layout

--- TastyCourse_swiftUI/TastyCourse_swiftUI/ContentView.swift
+++ TastyCourse_swiftUI/TastyCourse_swiftUI/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(spacing: 30) {
+            Text("Authorisation")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title2.bold())
+                .background(Color("whiteAlpha"))
+                .cornerRadius(30)
+            
+            VStack {
+                TextField("Enter email", text: $email)
+                    .padding()
+                    .background(Color("whiteAlpha"))
+                    .cornerRadius(12)
+                    .padding(5)
+                    .padding(.horizontal,12)
+                SecureField("Enter password", text: $password)
+                    .padding()
+                    .background(Color("whiteAlpha"))
+                    .cornerRadius(12)
+                    .padding(5)
+                    .padding(.horizontal,12)
+                Button{
+                    print("authorization")
+                } label: {
+                  Text("Log in")
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(LinearGradient(colors: [
+                    Color("yellow"),Color("orange")
+                ], startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(12)
+                .padding(5)
+                .padding(.horizontal,12)
+                .font(.title3.bold())
+                .foregroundColor(.white)
+                
+                Button{
+                    print("Вы еще не с нами?")
+                } label: {
+                  Text("Вы еще не с нами?")
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity)
+                .cornerRadius(12)
+                .padding(5)
+                .padding(.horizontal,12)
+                .font(.title3.bold())
+                .foregroundColor(Color(.brown))
+            }
+            .padding()
+            .background(Color("whiteAlpha"))
+            .cornerRadius(24)
+            .padding(20)
+            
+            //.padding(.vertical)
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Image("pizza"))
+            .ignoresSafeArea()
     }
 }
 
