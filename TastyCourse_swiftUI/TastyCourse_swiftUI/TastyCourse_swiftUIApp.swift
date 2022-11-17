6e5a385397e8c4ae3288326ed309c01a1369695b
fedir72 <fedir72@ua.fm>
2022-11-17 21:52:35 +0200

    empty project

---
+++ TastyCourse_swiftUI/TastyCourse_swiftUI/TastyCourse_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyCourse_swiftUIApp.swift
+//  TastyCourse_swiftUI
+//
+//  Created by Fedii Ihor on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TastyCourse_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
